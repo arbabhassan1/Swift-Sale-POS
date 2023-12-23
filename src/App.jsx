0f813f232c0db5af0d297e62a1ffe1c004122348
serverpc <linkto898@hotmail.com>
2023-12-23 20:49:40 +0500

    Add Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
 
 function App() {
   return (
-
-<>
-  
-</>
-
-
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index/>} />
+        <Route path="/home" element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
